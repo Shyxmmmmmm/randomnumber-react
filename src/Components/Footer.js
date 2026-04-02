8bd40193470142215af2964091864193d85d60a1
shyxmmmmmm <shyamkumar2005464@gmail.com>
2026-04-02 14:03:42 +0530

    updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,9 @@
+
+
 const Footer = () => {
     return (
-        <div className="bg-white mt-10 flex py-6 pl-5 justify-between items-center">
-            <div className="flex flex-col gap-2">
-                <h1 className="font-semibold lg:text-sm md:text-sm text-xs">Build with React and Tailwind CSS</h1>
-            </div>
+        <div className="bg-white mt-10 flex py-6 px-3 justify-between items-center">
+            <h1 className="font-semibold lg:text-sm md:text-sm text-xs">Build with React and Tailwind CSS</h1>
             <div className="hidden lg:block md:block">
                 <div className="flex  lg:gap-10 md:gap-10 gap-2">
                     <i class="fa-brands fa-instagram fa-lg " style={{ color: 'rgb(22, 22, 22)' }}></i>
@@ -11,7 +11,7 @@ const Footer = () => {
                     <i class="fa-brands fa-skyatlas fa-lg " style={{ color: 'rgb(22, 22, 22)' }}></i>
                 </div>
             </div>
-            <h1 className="lg:text-md md:text-md text-xs">&copy; 2026 Random Number Generator</h1>
+            <h1 className="lg:text-md md:text-md text-xs">&copy; 2026 Counter Dashboard</h1>
         </div>)
 }
 
